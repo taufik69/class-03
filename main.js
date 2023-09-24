9366905ae3ae26e3e6b4264f5b8f7b32899f0d6c
taufik69 <taufik35-2915@diu.edu.bd>
2023-09-25 00:32:31 +0600

    string all method

--- main.js
+++ main.js
@@ -1,4 +1,136 @@
 /**
- * todo :
+ * todo : data type on js
+ * todo : primitve and non primitve
+ * todo : primitve
+ * todo : non-primitve  --> array , object , function
  *
  */
+
+// 1st step
+
+// let MyFirstArray = new Array();
+
+// let nam = "taufik";
+// let number = 23;
+
+// let myFirstArray = ["taufik", 2342, true, "world", "hello", false];
+// let jsPeople = [
+//     "mohima ",
+//     "jahid ",
+//     "redowan",
+//     "Mushira ",
+//     "Nusrat",
+//     "Mahya",
+//     "Tamzid",
+//     "taufik",
+//   ];
+
+// console.log(jsPeople[]);
+// myFirstArray[0] = "Tasin sultana sorna";
+// myFirstArray[1] = "apatoto keo nai";
+// myFirstArray[9] = false;
+
+// myFirstArray.shift();
+// myFirstArray.unshift(37948598);
+
+// myFirstArray.pop();
+// myFirstArray.push("sadkfj");
+
+// myFirstArray.splice(3, 1);
+// let newarray = myFirstArray.join(" <---1--> ");
+// delete myFirstArray[0];
+// add 2 array
+
+// console.log(myFirstArray.concat(jsPeople));
+// console.log(jsPeople);
+
+// ----> loop ----> for loop
+
+// 1st step initialization let i = 0;
+// 2nd step conditon checking i < 100;
+// 3rd step increment and decrement i++ , --i
+
+// for (let i = 2024; i >= 1; i--) {
+//   console.log(i + "taufik");
+// }
+
+// namta program
+
+// 10 X 1 = 10
+// 10 X 10 = 100
+// let y = 10;
+// let x = `hello ${y} `;
+// let x = "hello " + y;
+// let namta = 5000000000;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${namta} X ${i} = ${namta * i}`);
+// }
+
+/**
+ * todo : assignment : ekta array thakbe , array er moode 10 ta element thakbe ,
+ * todo : 10 ta element browser show korben using loop
+ * todo : let [1,2,3     10  , 49]
+ * todo : array.lenght  --> show total item number
+ * todo : 1 ta for loop
+ * todo : condition a array.length i < array.length
+ * todo : loop er execution body te total array item list golan show koraben
+ * todo : arr[0] ---> show the array item
+ */
+
+// let jsPeople = [
+//     "mohima ",
+//     "jahid ",
+//     "redowan",
+//     "Mushira ",
+//     "Nusrat",
+//     "Mahya",
+//     "Tamzid",
+//     "taufik",
+//   ];
+
+//   jsPeople[0] ----> "mohima"
+
+/**
+ *
+ * todo : previous class we discuss some method of array
+ * todo : today we discuss some string built in method
+ * todo : lastly we have enough time then we make a if else if conditon and switch conditon
+ *
+ */
+
+// let x = "ABCDEFGH";
+// let y = "Mir Faysal Sir";
+// console.log(y.charAt(4));
+// console.log(y.charCodeAt(1));
+
+/**
+ * todo : slice(startPoint , endPoint)
+ * todo : subString(startPoint , endPoint)
+ * todo : replace("target" , "replace value")
+ * todo : trim()
+ * todo : toUpperCase()
+ * todo : toLowerCase ()
+ * todo : concat()
+ */
+
+// console.log(y.slice(0, 5));
+// console.log(y.substring(3, 10));
+// console.log(y.substr(3));
+
+// let x = "I LOVE JAVASCRIPT";
+// let y = "i love javascript";
+// let result = x.replaceAll("love", "hate");
+// let uppercase = x.toUpperCase();
+// let convertLowercase = x.toLowerCase();
+// let convertLowercase = x.toLocaleLowerCase();
+// let trim = x.trim();
+// let trim = x.trimEnd();
+// console.log(trim);
+
+// concat method in string
+
+// let x = "taufik";
+// let y = " sorna";
+
+// let addingEx = x.concat(y);
+// console.log(addingEx);
